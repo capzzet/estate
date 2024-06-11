b666e8d4b22115cd64a460b7ef8c48bed143b03e
timur <capzzet@mail.ru>
2024-06-11 22:02:55 +0600

    firt try

---
+++ real-estate-agency/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\BuildingsController;
+use App\Http\Controllers\SaleController;
+use App\Http\Controllers\ServicesController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactsController;
+use App\Http\Controllers\ReviewsController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog');
+Route::get('/buildings', [BuildingsController::class, 'index'])->name('buildings');
+Route::get('/for-sale', [SaleController::class, 'index'])->name('for-sale');
+Route::get('/services', [ServicesController::class, 'index'])->name('services');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/contacts', [ContactsController::class, 'index'])->name('contacts');
+Route::get('/reviews', [ReviewsController::class, 'index'])->name('reviews');
---
+++ real-estate-agency/webpack.mix.js
@@ -0,0 +1,2 @@
+mix
+    .copy('resources/fonts/Rubik-VariableFont_wght.ttf', 'public/fonts');
